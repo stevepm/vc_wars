39b6221b9cc58cbdf80f4364b34560282d9822db
stevepm <steven.magelowitz@gmail.com>
2014-05-10 16:33:57 -0600

    Updated user interactions

--- app.rb
+++ app.rb
@@ -1,55 +1,93 @@
-end_game = false
-until end_game
-  city_chosen = false
-  until city_chosen
-    puts 'Choose a city'
-    puts 'Austin, Vegas, NY, SF, Boulder'
-    input = gets.chomp.downcase
-    if input == 'austin'
-      puts 'You have $5.00 in the bank'
-      city_chosen = true
-    elsif input == 'vegas'
-      puts 'You have $10.00 in the bank'
-      city_chosen = true
-    elsif input == 'ny'
-      puts 'You have $20.00 in the bank'
-      city_chosen = true
-    elsif input == 'sf'
-      puts 'You have $35.00 in the bank'
-      city_chosen = true
-    elsif input == 'boulder'
-      puts 'You have $8.00 in the bank'
-      city_chosen = true
-    else
-      puts 'Choose a correct city, dipshit'
-    end
-  end
+require_relative 'lib/user'
 
-  puts User.info
+@end_game = false
 
+def buy_options(user)
+  puts 'buy'
+end
 
-  end_game = true
+def fly_options(user)
+  puts 'fly'
+end
+
+def wait(user)
+  puts 'wait'
+end
+
+def manage_assets(user)
+  puts 'manage'
 end
 
 
-user_hash = {
-  city: 'x',
-  capital: 'y',
-  assets: [{company: 'google', value: 'x'}, ''],
+def game_options(user)
+  option_chosen = false
+  puts 'Please choose your next option:'
+  until option_chosen
+    puts 'Buy companies [buy]'
+    puts 'Fly to a new city [fly]'
+    puts 'Wait a day [wait]'
+    puts 'Manage Assets [mng]'
+    puts 'Quit [quit]'
+    option = gets.chomp.downcase
+    case option
+      when 'buy'
+        buy_options(user)
+        option_chosen = true
+      when 'fly'
+        fly_options(user)
+        option_chosen = true
+      when 'wait'
+        wait(user)
+        option_chosen = true
+      when 'mng'
+        manage_assets(user)
+        option_chosen = true
+      when 'quit'
+        option_chosen = true
+        @end_game = true
+      else
+        puts 'Please enter a correct option'
+    end
+  end
+end
 
-}
+def choose_city
+  city_chosen = false
+  puts 'Choose a city'
+  until city_chosen
+    puts 'Austin, Vegas, NY, SF, Boulder'
+    city = gets.chomp.downcase
+    case city
+      when 'austin'
+        city_chosen = true
+      when 'vegas'
+        city_chosen = true
+      when 'ny'
+        city_chosen = true
+      when 'sf'
+        city_chosen = true
+      when 'boulder'
+        city_chosen = true
+      else
+        puts 'Choose a correct city, dipshit'
+    end
+  end
+  city
+end
 
-game_hash = {
-  companies: [{company: 'google', location: 'NY', value: 'y', genre: 'o', company_info: []}]
-}
+def display_user_info(user)
+  puts "Capital : #{user.user_info[:capital]}"
+  puts "Net Worth : #{user.user_info[:net_worth]}"
+  puts "Current City : #{user.user_info[:city]}"
+end
 
-company_options = {
-  ceo: [{name: 'steve', companyvalue: company_value += 'x-y%', cost: 'y'}],
-  marketing: [{media_type: 'stuff', companyvalue: company_value += 'x-y%', cost: 'y'}],
-  growth: [{cost_per_employee: '123'}],
-  sell: [{company_worth: '12312'}]
-}
+until @end_game
 
-event = {
+  user = User.new(choose_city)
+  display_user_info(user)
+  game_options(user)
 
-}
+  @end_game = true
+end
+
+
